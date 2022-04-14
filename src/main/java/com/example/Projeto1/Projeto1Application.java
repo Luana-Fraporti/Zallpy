4343be897d4baab74e52dc5c28c342c230cb8dea
matheusrocha <matheus.rocha@zallpy.com>
2022-04-14 13:29:31 -0300

    melhoria na performance do código

--- src/main/java/com/example/Projeto1/Projeto1Application.java
+++ src/main/java/com/example/Projeto1/Projeto1Application.java
@@ -5,27 +5,29 @@ import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class Projeto1Application {
 	public static void main(String[] args) {
-		int tam;
-		int x = 0;
-		String palavra;
+
 		String saida1 = "";
 		String saida2 = "";
-		palavra = ("A diva em Argel alegra-me a vida");
-		tam = palavra.length();
+		String palavra = ("Olé! Maracujá, caju, caramelo");
+		int tam = palavra.length();
+		int x = 0;
 
 		do { // verifica se caracter é letra ou numero
 			if((Character.isLetter(palavra.charAt(x)))||(Character.isDigit(palavra.charAt(x))))
 				saida1 += palavra.charAt(x);
 			x++;
 		} while(x < tam);
-
-		for(int y = (saida1.length()-1); y >= 0; y--)	// inversão da saida1
+		tam = saida1.length()-1;
+		saida1 = saida1.toLowerCase();
+		for(int y = (tam); y >= 0; y--)	// inversão da saida1
 			saida2 += saida1.charAt(y);
 
-		if((saida1.toLowerCase()).equals(saida2.toLowerCase())){	// compara as saidas - trasformação de tamaho e compara
+		if((saida1).equals(saida2)){	// compara as saidas - trasformação de tamaho e compara
 			System.out.println("É PALÍNDROMO!!!");
 		}else{
 			System.out.println("NÃO É PALÍNDROMO!!!");
